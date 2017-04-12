797b46291449e388a2084ac8c5a98fe7f3c7f207
baocaixiong <baocaixiong@gmail.com>
2017-04-12 17:15:03 +0800

    nothing

--- src/main/java/org/thin/ugc/filters/MyFilter.java
+++ src/main/java/org/thin/ugc/filters/MyFilter.java
@@ -22,7 +22,7 @@ class MyFilter implements Filter
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
     {
-        logger.info("doing my filter");
+        logger.info("doing my filter.");
         chain.doFilter(request, response);
     }
 
